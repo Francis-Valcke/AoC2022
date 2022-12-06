ce3d9ce2d9d6d33c73e27a2049b241fc9d775d4e
Francis Valcke <fvalcke@hubspot.com>
2022-12-06 22:54:27 +0100

    day6

--- AoC2022-6/app/Main.hs
+++ AoC2022-6/app/Main.hs
@@ -6,12 +6,27 @@ import Debug.Trace
 
 main = do
   file <- readFile "input1b.txt"
-  print $ (analyze file 14)
+  print $ (analyze 14 file)
 
-analyze::String->Int->Int
-analyze stream windowSize = length $ substring stream
+
+analyze::Int->String->Int
+analyze windowSize stream = length $ substring stream
         where substring str =
                 let window = take windowSize str
                 in  if windowSize == (length . nub $ window) then window++[] else (head str):substring (drop 1 str)
+
+
+-- Meh
+-- analyze::Int->String->Int
+-- analyze windowSize stream =
+--                       let calcNewAcc (_,oldWindow) newValue =
+--                                         let newWindoww = (drop 1 oldWindow) ++ [newValue]
+--                                             newWindow = trace newWindoww newWindoww
+--                                         in  if windowSize /= (length . nub $ newWindow)
+--                                             then (1, newWindow)
+--                                             else (0, newWindow)
+--                           (firstWindow, rest) = splitAt windowSize stream
+--                           in (+) 4 $ length . takeWhile (\(i,_)-> i==1) $ scanl calcNewAcc (1,firstWindow) rest
+
 
 
