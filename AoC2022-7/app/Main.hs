11dc648b7be7d93c72b48ca818b589b1fe15989a
Francis Valcke <fvalcke@hubspot.com>
2022-12-27 16:13:13 +0100

    Day7

--- AoC2022-7/app/Main.hs
+++ AoC2022-7/app/Main.hs
@@ -2,41 +2,60 @@ module Main where
 import Data.Char (isNumber)
 import Debug.Trace
 import Text.Printf
-import Data.List (foldl', isPrefixOf)
+import Data.Char
+import Data.List (foldl', isPrefixOf, nub, sort)
 
 main :: IO ()
 main = do
-  file <- readFile $ "input1a.txt"
+  file <- readFile $ "input1b.txt"
   let inputLines = lines file
-  let test = evaluateLine inputLines [] 0
-  print $ takeScope inputLines [] []
+  let associated = associateWithPath inputLines []
+  let invertedPaths = invertPaths associated
+  let dirs = distinctPaths invertedPaths
+  let filteredFiles = filterFiles invertedPaths
+  let dirSizes = calcSizes dirs filteredFiles
+  -- Part 1
+  let part1 = sum . map (\(_,size) -> size) $ filter (\(dir, size) -> size <= 100000) dirSizes
+  putStrLn $ "Part1: " ++ show part1
+  -- Part 2
+  let rootDirSize = snd . head $ dirSizes
+  let sizeToBeDeleted = 30000000 - (70000000 - rootDirSize)
+  let extractedDirSizes = map (\(_,size)-> size) dirSizes
+  let sizeToDelete = head . filter (\dirSize -> dirSize > sizeToBeDeleted) $ sort extractedDirSizes
+  putStrLn $ "Part2: " ++ show sizeToDelete
 
+calcSizes::[[String]]->[(Int, [String])]->[([String],Int)]
+calcSizes dirs files = map (\dirPath -> (dirPath,calcSize dirPath files)) dirs
+                where calcSize::[String]->[(Int, [String])]->Int
+                      calcSize path files = sum . map (\(size,_)->size) $ filter (\(size, filePath) -> path `isPrefixOf` filePath) files
 
--- rest of file -> current path -> skipLines -> size
-evaluateDir::[String]->[String]-> Int
-evaluateDir restOfFile path = 0
-    -- let lineToEvaluate = head $ drop skipLines file
-    -- in parse lineToEvaluate
-    -- where parse line
-    --         | "$ cd" `isPrefixOf` line =
-    --                       let target = drop 4 line
-    --                       in case target of ".." ->
-    --                                         other ->
-    --         | "$ ls" `isPrefixOf` line = evaluateLine rest currPath counter
-    --         | "dir" `isPrefixOf` line = evaluateLine rest currPath counter
-    --         | otherwise =
--- evaluateLine (line:[]) currPath counter = read . head . words $ line
+distinctPaths::[(a, [String])]->[[String]]
+distinctPaths input =
+                let paths = map (\(_,x)->x) input
+                in nub paths
 
--- file -> prefix -> currentPath ->
-takeScope::[String]->[String] -> [String]->[String]
-takeScope (line:rest) pathPrefix path =
-              let newPath = calcNewPath
-              in if pathPrefix == (take (length pathPrefix) [])
-                 then line : takeScope rest pathPrefix newPath
-                 else line : []
-              where calcNewPath
-                      | "$ cd" `isPrefixOf` line =
-                                    let target = drop 4 line
-                                    in case target of ".." -> drop 1 path
-                                                      other -> other : path
-                      | otherwise = path
+invertPaths::[(a, [String])]->[(a, [String])]
+invertPaths xs = map invertPath xs
+                where invertPath (f,path) = (f, reverse path)
+
+filterFiles::[(String, [String])]-> [(Int, [String])]
+filterFiles associatedPaths = map toNum $ filter isFile associatedPaths
+              where isFile (f, _) = isDigit . head . head . words $ f
+                    toNum (f, xs) = (read . head. words $ f, xs)
+
+associateWithPath::[String]->[String]->[(String, [String])]
+associateWithPath (line:rest) currPath =
+                      let newPath = calcPath line currPath
+                      in (line, newPath):(associateWithPath rest newPath)
+associateWithPath (line:[]) currPath =
+                      let newPath = calcPath line currPath
+                      in (line, newPath):[]
+associateWithPath [] currPath = [("nope", ["nope"])]
+
+calcPath::String->[String]->[String]
+calcPath line currPath
+            | "$ cd " `isPrefixOf` line =
+                      let path = drop 5 line
+                      in case path of ".." -> drop 1 currPath
+                                      xs -> xs : currPath
+            | otherwise = currPath
